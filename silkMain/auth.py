d1ed68d18a8ca2e005594db41f393c75e177ea7b
Khiem Nguyen <kevin314pi@gmail.com>
2025-10-24 02:20:49 -0500

    Added small modifications
    - added timestamp for last modification
    - sort notes by newest first
    - fixed register autologin issue
    - switched to email rather than username
    - removed requirement for a title, added untitled placeholder instead

--- silkMain/auth.py
+++ silkMain/auth.py
@@ -15,26 +15,27 @@ bp = Blueprint('auth', __name__, url_prefix = '/auth')
 @bp.route('/register', methods = ('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         db = get_db()
         error = None
 
-        if not username:
-            error = 'Username is required'
+        if not email:
+            error = 'email is required'
         elif not password:
             error = 'Password is required'
         
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password + username)),
+                    "INSERT INTO user (email, password) VALUES (?, ?)",
+                    (email, generate_password_hash(password + email)),
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f"User {username} is already registered"
+                error = f"This email is already in use"
             else:
+                session.clear()
                 return redirect(url_for("auth.login"))
             
         flash(error)
@@ -44,18 +45,18 @@ def register():
 @bp.route('/login', methods = ('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+            'SELECT * FROM user WHERE email = ?', (email,)
         ).fetchone()
 
         if user is None:
-            error = 'Incorrect username and/or password'
-        elif not check_password_hash(user['password'], password+username):
-            error = 'Incorrect username and/or password'
+            error = 'Incorrect email and/or password'
+        elif not check_password_hash(user['password'], password+email):
+            error = 'Incorrect email and/or password'
         
         if error is None:
             session.clear()
--- silkMain/note.py
+++ silkMain/note.py
@@ -2,100 +2,102 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
+from datetime import datetime
 
 from silkMain.auth import login_required
 from silkMain.db import get_db
 
 bp = Blueprint('note', __name__)
 
-def get_note(id, check_author=True):
-    note = get_db().execute(
-        'SELECT p.id, title, body, created, owner_id, username'
-        ' FROM note p JOIN user u ON p.owner_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if note is None:
-        abort(404, f"Note id {id} doesn't exist.")
-
-    if check_author and note['owner_id'] !=g.user['id']:
-        abort(403)
-
-    return note
-
 # views
 
+# default view
 @bp.route('/')
+@login_required
 def index():
     db = get_db()
     notes = db.execute(
-        'SELECT n.id, title, body, created, owner_id, username'
-        ' FROM note n JOIN user u ON n.owner_id = u.id'
+        'SELECT n.id, title, body, created, modified, owner_id'
+        ' FROM note n'
         ' WHERE n.owner_id = ?'
-        ' ORDER BY created DESC',
+        ' ORDER BY modified ASC',
         (g.user['id'],)
     ).fetchall()
     return render_template('notes/index.html', notes=notes)
 
-# create blueprint
+# create view
 @bp.route('/create', methods=('GET','POST'))
 @login_required
 def create():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        error = None
 
         if not title:
-            error = 'Title is requred.'
-        
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO note (title, body, owner_id)'
-                ' VALUES (?,?,?)',
-                (title, body, g.user['id'])
-            )
-            db.commit()
-            return redirect(url_for('note.index'))
+            title = 'Untitled'
+
+        db = get_db()
+        db.execute(
+            'INSERT INTO note (title, body, owner_id)'
+            ' VALUES (?,?,?)',
+            (title, body, g.user['id'])
+        )
+        db.commit()
+        return redirect(url_for('note.index'))
     return render_template('notes/create.html')
 
-# update blueprint
+# update view
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
-    note = get_note(id)
+    note = get_db().execute(
+        'SELECT n.id, title, body, created, modified, owner_id'
+        ' FROM note n'
+        ' WHERE n.id = ?',
+        (id,)
+    ).fetchone()
+
+    if note is None:
+        abort(404, f"Note id {id} doesn't exist.")
+
+    if note['owner_id'] !=g.user['id']:
+        abort(403)
 
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        error = None
-
-        if not title:
-            error = 'Title is requred.'
         
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE note SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for('note.index'))
+        if not title:
+            title = 'Untitled'
+
+        db = get_db()
+        db.execute(
+            'UPDATE note SET title = ?, body = ?, modified = ?'
+            ' WHERE id = ?',
+            (title, body, datetime.now().isoformat(), id)
+        )
+        db.commit()
+        return redirect(url_for('note.index'))
     return render_template('notes/update.html', note=note)
 
-# delete blueprint
+# delete view
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
-    get_note(id)
+    note = get_db().execute(
+        'SELECT n.id, owner_id'
+        ' FROM note n'
+        ' WHERE n.id = ?',
+        (id,)
+    ).fetchone()
+
+    if note is None:
+        abort(404, f"Note id {id} doesn't exist.")
+
+    if note['owner_id'] !=g.user['id']:
+        abort(403)
     db = get_db()
     db.execute('DELETE FROM note WHERE id = ?', (id,))
     db.commit()
+    flash('Note successfully deleted')
     return redirect(url_for('note.index'))
